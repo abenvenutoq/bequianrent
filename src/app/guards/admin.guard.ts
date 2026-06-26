0a1467aedfee01971677be3052ccbe380d6f1189
abenvenutoq <a.benvenuto@duocuc.cl>
2026-06-26 17:44:40 -0400

    Update 19: Cambios en comentarios de admin.guard

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,17 +1,22 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.services'; // Ajusta la ruta a tu servicio
+import { AuthService } from '../services/auth.services'; 
 
+
+/**
+ * @description
+ * Guard para protección doble en paginas solo para administrador
+ */
 export const adminGuard = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  // Reutilizamos tu función estrella ⭐
+
   if (authService.esAdmin()) {
-    return true; // Lo dejamos pasar sin problemas
+    return true; 
   }
 
-  // Si no es admin, bloqueamos la entrada y lo mandamos al login
+  /** Si no es admin redirige al login */
   router.navigate(['/login']);
   return false;
 };
